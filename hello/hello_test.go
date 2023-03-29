b3f760d94546da59b722ac758030a91e394a0538
Jan Polivka <j.polivka5@gmail.com>
2023-03-29 20:16:02 +0000

    working

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,6 +1,8 @@
 package hello
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGreeter(t *testing.T) {
 	got := Greeter()
@@ -13,5 +15,18 @@ func TestWorlder(t *testing.T) {
 	got := Worlder()
 	if got != "world" {
 		t.Errorf("did not get world")
+	}
+}
+
+func Test_helloWorld(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{",hello world"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			helloWorld()
+		})
 	}
 }
