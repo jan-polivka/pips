4aa71f98005fe10d643422a28084bf70c1540b25
Jan Polivka <j.polivka5@gmail.com>
2023-03-29 20:20:32 +0000

    working

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -22,7 +22,7 @@ func TestHelloWorld(t *testing.T) {
 	tests := []struct {
 		name string
 	}{
-		{"hello worl"},
+		{"hello, hello world"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
