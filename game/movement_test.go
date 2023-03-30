9fd4151bfe2351595ce591909d83884c567d1cc3
Jan Polivka <j.polivka5@gmail.com>
2023-03-30 20:28:03 +0000

    working

--- game/movement_test.go
+++ game/movement_test.go
@@ -10,5 +10,5 @@ func Test_spawnPip(t *testing.T) {
 	var board [6]int
 	var result = spawnPip(&board[0], 6)
 	var wrong [5]int
-	assert.Equal(t, wrong, result)
+	assert.Equal(t, &wrong[0], result)
 }
