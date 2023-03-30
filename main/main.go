3b9705952a233d92314ca0584d9a6cc2609df046
Jan Polivka <j.polivka5@gmail.com>
2023-03-30 20:48:09 +0000

    working

--- main/main.go
+++ main/main.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"math/rand"
+)
+
+type Board struct {
+	matrix []int
+	rand   *rand.Rand
+}
+
 func main() {
 
 	var board [6]int
+	r := rand.New(rand.NewSource(99))
 
 }
