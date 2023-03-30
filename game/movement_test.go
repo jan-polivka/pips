2a8acda60362d7e833da93db822383037df3759b
Jan Polivka <j.polivka5@gmail.com>
2023-03-30 20:19:58 +0000

    working

--- game/movement_test.go
+++ game/movement_test.go
@@ -1,1 +1,27 @@
 package game
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+
+func Test_spawnPip(t *testing.T) {
+	type args struct {
+		board *int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *int
+	}{
+		{"test", var board [6]int, board}
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := spawnPip(tt.args.board); got != tt.want {
+				t.Errorf("spawnPip() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
