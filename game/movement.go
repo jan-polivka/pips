390c70c709a537651b3729f572003598fb8db498
Jan Polivka <j.polivka5@gmail.com>
2023-03-31 20:12:09 +0000

    working

--- game/movement.go
+++ game/movement.go
@@ -10,6 +10,6 @@ func SpawnPip(board types.Board) types.Board {
 }
 
 func getSpawningPoint(rand *rand.Rand) int {
-	// return rand.Intn(5-0) + 0
-	return 3
+	return rand.Intn(5-0) + 0
+	// return 3
 }
