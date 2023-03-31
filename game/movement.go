9ad59d0b0f5bc94c6c1ff620db17251a841edb8f
Jan Polivka <j.polivka5@gmail.com>
2023-03-31 20:51:02 +0000

    working

--- game/movement.go
+++ game/movement.go
@@ -1,7 +1,6 @@
 package game
 
 import (
-	"fmt"
 	"math/rand"
 	"pips/types"
 )
@@ -9,7 +8,6 @@ import (
 func SpawnPip(board types.Board) types.Board {
 
 	spawningPoint := getSpawningPoint(board.Rand)
-	fmt.Println(spawningPoint)
 	board.Matrix[spawningPoint] = 1
 	return board
 }
