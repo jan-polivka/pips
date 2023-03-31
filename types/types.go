26f1586ffbff344d2f5c3526ec969b4f5fbd0606
Jan Polivka <j.polivka5@gmail.com>
2023-03-31 21:05:28 +0000

    working

--- types/types.go
+++ types/types.go
@@ -4,7 +4,14 @@ import (
 	"math/rand"
 )
 
+// I should remove the Matrix and replace it with x and y limits
+// Then add an array of pips
 type Board struct {
 	Matrix [10]int
 	Rand   *rand.Rand
+}
+
+type Pip struct {
+	Position [2]int
+	Team     int
 }
