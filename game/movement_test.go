87c04ae342991bd0a42b7a17626b435d91ffd768
Jan Polivka <j.polivka5@gmail.com>
2023-03-31 17:36:08 +0000

    working

--- game/movement_test.go
+++ game/movement_test.go
@@ -16,3 +16,9 @@ func Test_SpawnPip(t *testing.T) {
 	var result = SpawnPip(board)
 	assert.Equal(t, board, result)
 }
+
+func Test_GetSpawningPoint(T *testing.T) {
+	rand := rand.New(rand.NewSource(99))
+	result := getSpawningPoint(rand)
+	assert.Equal(0, result)
+}
