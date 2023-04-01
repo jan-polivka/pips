f0bfa353cd482353339da21f032f21cb9e7af65a
Jan Polivka <j.polivka5@gmail.com>
2023-04-01 08:01:09 +0000

    working

--- types/types.go
+++ types/types.go
@@ -7,9 +7,10 @@ import (
 // I should remove the Matrix and replace it with x and y limits
 // Then add an array of pips
 type Board struct {
-	Matrix [10]int
-	Pips   [1]Pip
-	Rand   *rand.Rand
+	History [10][10]int
+	Matrix  [10]int
+	Pips    [1]Pip
+	Rand    *rand.Rand
 }
 
 type Pip struct {
