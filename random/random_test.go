ded1a6e48dd5d07152f1bd31b2b4e82f9df75bad
Jan Polivka <j.polivka5@gmail.com>
2023-04-01 16:20:21 +0000

    working

--- random/random_test.go
+++ random/random_test.go
@@ -1,6 +1,7 @@
 package random
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,13 +10,14 @@ import (
 func Test_GenerateRandomInt(t *testing.T) {
 	const min = 0
 	const max = 100
-	const size = max - min
+	const size = max - min + 1
 	randMap := make(map[int]int)
 	for i := 0; i < 100*size; i++ {
 		result := GenerateRandomInt(min, max)
 		randMap[result] = 1
 	}
 
+	fmt.Println(randMap)
 	for i := min; i < max; i++ {
 		assert.NotEqual(t, 0, randMap[i])
 	}
