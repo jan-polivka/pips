f8964a9fba41db9f766fd68c179f40f4c9177eda
Jan Polivka <j.polivka5@gmail.com>
2023-04-01 15:13:52 +0000

    working

--- random/random.go
+++ random/random.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+type GenerateRandomIntFunction func(int, int) int
+
 func GenerateRandomInt(min int, max int) int {
 	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return rand.Intn(max-min) - min
