c58d6422b8f1c3ec1fa08353c7b5efe140b73d8a
Jan Polivka <j.polivka5@gmail.com>
2023-04-01 16:04:41 +0000

    working

--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"pips/game"
 	"pips/random"
@@ -12,6 +13,15 @@ func main() {
 	rand := rand.New(rand.NewSource(99))
 	matrix := [10]int{}
 	board := types.Board{Matrix: matrix, Rand: rand}
-	game.SpawnPip(board, random.GenerateRandomInt)
-
+	board = game.SpawnPip(board, random.GenerateRandomInt)
+	board = game.MovePip(board, 0, random.GenerateRandomInt)
+	fmt.Println(board)
+	board = game.MovePip(board, 0, random.GenerateRandomInt)
+	fmt.Println(board)
+	board = game.MovePip(board, 0, random.GenerateRandomInt)
+	fmt.Println(board)
+	board = game.MovePip(board, 0, random.GenerateRandomInt)
+	fmt.Println(board)
+	board = game.MovePip(board, 0, random.GenerateRandomInt)
+	fmt.Println(board)
 }
