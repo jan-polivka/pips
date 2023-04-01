a694600b8161dafa15ef9f8f672f25ab856eabbe
Jan Polivka <j.polivka5@gmail.com>
2023-04-01 08:09:34 +0000

    working

--- types/types.go
+++ types/types.go
@@ -7,7 +7,7 @@ import (
 // I should remove the Matrix and replace it with x and y limits
 // Then add an array of pips
 type Board struct {
-	History [][10]int
+	History BoardSnapshot
 	Matrix  [10]int
 	Cols    int
 	Pips    [1]Pip
