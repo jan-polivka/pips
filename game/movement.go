293c9645659bc2271e1a409dda24860c4695dc42
Jan Polivka <j.polivka5@gmail.com>
2023-04-01 08:31:13 +0000

    working

--- game/movement.go
+++ game/movement.go
@@ -9,6 +9,7 @@ func SpawnPip(board types.Board) types.Board {
 	spawningPoint := getSpawningPoint(board.Rand)
 	board.Matrix[spawningPoint] = 1
 	// pip := types.Pip{Position: spawningPoint, Team: 1}
+	// board.Pips = append(board.Pips, pip)
 	return board
 }
 
