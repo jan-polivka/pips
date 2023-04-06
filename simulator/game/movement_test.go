e94e45a8f712df883aea12497f8f3608e92c1522
Jan Polivka <j.polivka5@gmail.com>
2023-04-06 17:53:16 +0000

    working

--- simulator/game/movement_test.go
+++ simulator/game/movement_test.go
@@ -148,7 +148,7 @@ func Test_spawnTwoPipsAndMoveThem(t *testing.T) {
 	}
 	assert.NotEqual(t, teamOne, newBoard.Matrix[posOne])
 	assert.NotEqual(t, teamTwo, newBoard.Matrix[posTwo])
-	// assert.Equal(t, teamOne, newBoard.Matrix[posOne+2])
+	assert.Equal(t, teamOne, newBoard.Matrix[posOne+2])
 }
 
 func Test_GetBoundsTeamOne(t *testing.T) {
