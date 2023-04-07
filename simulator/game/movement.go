75426cbabbcb8ad0451c86ad99577d81987cd1be
Jan Polivka <j.polivka5@gmail.com>
2023-04-07 12:03:15 +0000

    working

--- simulator/game/movement.go
+++ simulator/game/movement.go
@@ -26,7 +26,7 @@ func MovePip(board types.Board, pipIdx int, getRandomInt random.GenerateRandomIn
 }
 
 func getNextPosition(pip types.Pip, getRandomInt random.GenerateRandomIntFunction) int {
-	newDirection := getRandomInt(-1, 2)
+	newDirection := getRandomInt(-1, 1)
 	if newDirection == 0 {
 		return pip.Position
 	}
