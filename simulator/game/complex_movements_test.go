d4032852db4717d0a49c9a8b91487c17ac9986ac
Jan Polivka <j.polivka5@gmail.com>
2023-04-07 08:39:12 +0000

    test update

--- simulator/game/complex_movements_test.go
+++ simulator/game/complex_movements_test.go
@@ -33,7 +33,7 @@ func (suite *TestSuite) SetupTest() {
 
 func (suite *TestSuite) Test_moveTwoPips() {
 	randFunc := func(int, int) int { return 1 }
-	newBoard := MovePip(suite.board, 0, randFunc)
+	// newBoard := MovePip(suite.board, 0, randFunc)
 	// var currPip int
 	// for i := 0; i < 3; i++ {
 	// 	if i%2 == 0 {
